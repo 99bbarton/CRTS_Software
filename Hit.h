2355c9a0b0e0a161f8fd52e332fb46356b499e64
Benjamin_N_Barton <99bbarton@gmail.com>
2019-02-26 11:44:19 -0500

    Implemented Hit class - still need to know format of CRV event class and code to calculate PE yield

--- Hit.h
+++ Hit.h
@@ -4,7 +4,6 @@
 #define HIT_H
 
 #include <CRVEvent>
-#include "Track.h"
 
 class Hit{
  public:
@@ -12,13 +11,14 @@ class Hit{
   Hit(CRVEvent,double pathL);
   ~Hit();
   double get_PEyield();
-  double get_PathLength();
+  double get_PathLength(); //In mm
   double get_PE_per_cm();
   
  private:
+  int trigNum; //Trigger number corresponding to the hit
   double PEyield; //Number of Photo electrons detected
-  double pathLength; //Path length of muon track through a counter in m
-  double calcPEyield(); //Calculate PE yield of hit
+  double pathLength; //Path length of muon track through a counter in mm
+  double calcPEyield(CRVEvent); //Calculate PE yield of hit
 };
 
 #endif
